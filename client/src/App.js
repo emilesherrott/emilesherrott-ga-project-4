8622e39e5229941c28f6c4297bca1e9d92f4db65
emilesherrott <emilesherrott@gmail.com>
2021-07-23 10:09:15 +0100

    Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-    useEffect(() => {
-        const getData = async () => {
-            const res = await fetch('/api/crops') // * <-- replace with your endpoint
-            const data = await res.json()
-            console.log(data)
-        }
-        getData()
-    })
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get('/api/crops')
+      console.log(res)
+    }
+    getData()
+  })
 
-    return <h1>Hello World</h1>
+  return <h1>Hello World</h1>
 }
 
 export default App
