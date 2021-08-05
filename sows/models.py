f64a6364215d28dead4600ce675f33c09f0a0141
emilesherrott <emilesherrott@gmail.com>
2021-08-05 12:08:06 +0100

    ready to deploy

--- sows/models.py
+++ sows/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Sow(models.Model):
-    month = models.CharField(max_length=20)
+    month = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return f"{self.month}"
